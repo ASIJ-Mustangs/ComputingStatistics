bceec1d45ee6d66ead283009248f1b9ea4471b9b
jstephensasij <45579926+jstephensasij@users.noreply.github.com>
2021-02-01 15:46:18 +0900

    Add files via upload

---
+++ ComputingStatistics.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class ComputingStatistics {
+   /**
+   * The ArrayList containing all of the loan data.
+   */
+   private ArrayList<Loan> data;
+   
+   /**
+    * Creates a new ComputingStatistics object with an empty ArrayList 
+    */
+   public ComputingStatistics() {
+      data = new ArrayList<Loan>();
+   }
+   
+   /**
+    * Creates a new ComputingStatistics object with the data passed in
+    */
+   public ComputingStatistics(ArrayList<Loan> d) {
+      data = d;
+   }
+   
+   /**
+    * Calclates the total amount funded from all of the loans in the file.
+    * @return the total loan amount.
+    */
+   public double totalAmount() {
+      double amount = 0.0;
+      for(int i = 0; i < data.size(); i++) {
+         amount = amount + data.get(i).getLoanAmount();
+      }
+      return amount;
+   }
+}
